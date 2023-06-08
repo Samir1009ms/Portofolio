fc4096f768705d777fc9be55f14130cb82285397
Samir1009ms <yusifovs1009@gmail.com>
2023-06-08 14:27:56 +0400

    Portofolio

--- src/data/ProjectData.ts
+++ src/data/ProjectData.ts
@@ -7,7 +7,7 @@ export const experiences: IExperience[] = [{
     time: "20 February 2023 - Now",
     header: "Mentor",
     contents: "Front end developer Mentor in Coders Azerbaijan",
-    skils: ["HTML5", "CSS", "SCSS", "Bootstrap", "React", "Redux Toolkit"]
+    skils: ["HTML5", "CSS", "SCSS", "Bootstrap", "React", "Redux", "Redux Toolkit"]
 
 }]
 
@@ -16,7 +16,7 @@ export const educations: IExperience[] = [
         time: "2022 - 2023",
         header: "Coders Azerbaijan",
         contents: "Front-end Developer Bootcamp",
-        skils: ["HTML5", "CSS", "SCSS", "Bootstrap", "React", "Redux Toolkit"]
+        skils: ["HTML5", "CSS", "SCSS", "Bootstrap", "React", "Redux", "Redux Toolkit"]
     },
     {
         time: '2018-2022',
